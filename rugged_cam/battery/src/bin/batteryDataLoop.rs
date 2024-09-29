efabe03880f7cd28c47026b73ab3c2b2dd12e51c
Jett Hays <jettblu.fabrications@gmail.com>
2024-09-29 01:02:29 -0700

    Update health logs

--- rugged_cam/battery/src/bin/batteryDataLoop.rs
+++ rugged_cam/battery/src/bin/batteryDataLoop.rs
@@ -1,4 +1,4 @@
-use log::{ info, LevelFilter };
+use log::{ info, error, LevelFilter };
 use battery::get_battery_parcentage;
 use std::time::SystemTime;
 use tokio::time;
@@ -8,16 +8,22 @@ pub async fn main() {
     const LATENCY_SECS: u64 = 120;
     let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
     // create unique health log file for each boot
-    let _ = simple_logging::log_to_file(format!("logs/health/{now}.log"), LevelFilter::Info);
+    let res = simple_logging::log_to_file(format!("logs/health/{now}.log"), LevelFilter::Info);
+    if res.is_err() {
+        panic!("Unable to create log file");
+    }
     loop {
         let battery_percentage: Result<f32, &'static str> = get_battery_parcentage();
-        if !battery_percentage.is_ok() {
-            panic!("Unable to fetch battery percentage");
-        }
-        let battery_percentage: f32 = battery_percentage.unwrap();
         // seconds since the epoch
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        info!("{} {}", now, battery_percentage);
-        time::sleep(time::Duration::from_secs(LATENCY_SECS)).await;
+        if !battery_percentage.is_ok() {
+            // if battery percentage is not fetched, log error
+            let error_msg = format!("{} Unable to fetch battery percentage", now);
+            error!("{}", error_msg);
+        } else {
+            let battery_percentage: f32 = battery_percentage.unwrap();
+            info!("{} {}", now, battery_percentage);
+            time::sleep(time::Duration::from_secs(LATENCY_SECS)).await;
+        }
     }
 }
