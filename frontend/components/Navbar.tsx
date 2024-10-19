ff1d3200cacd0f55e95ad1e3f0ab75a0f03b1683
Jett Hays <jettblu.fabrications@gmail.com>
2024-10-19 15:33:37 -0600

    Update icons

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
+import LogoAdventureLabs from "./LogoAdventureLabs";
 
 export default function Navbar() {
   return (
     <div>
       <Link href="/" className="hover:cursor-pointer">
-        <Image
-          src="/logoBeakerTree.png"
-          alt="Adventure Labs"
-          width={60}
-          height={60}
-          className="fixed top-10 left-6 z-20 invisible lg:visible"
-        />
+        <div className="absolute top-[-18px] left-[-16px] z-20">
+          <Image
+            src="/logoBeaker.svg"
+            alt="Adventure Labs"
+            width={140}
+            height={140}
+            className="invisible lg:visible"
+          />
+        </div>
       </Link>
-      <div className="ring ring-1 fixed top-10 right-10 rounded-md text-xl flex flex-row z-20 bg-gray-400/50 backdrop-blur-md">
+      <div className="ring ring-1 fixed top-10 right-10 rounded-md text-xl flex flex-row z-20 bg-gray-400/20 backdrop-blur-md">
         <Link
           href="/"
           className={`hover:cursor-pointer hover:bg-primary/20 px-4 py-2 rounded-tr-md rounded-br-md transition-colors duration-300`}
